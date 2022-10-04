41ff12a2cf3d84ec2d46a8c830c5fd392760ed51
Kradon Zhao <kradonzhao@gmail.com>
2022-10-04 09:26:26 -0400

    add fling detection

--- app/src/main/java/com/example/w4_p0b/MainActivity.java
+++ app/src/main/java/com/example/w4_p0b/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.w4_p0b;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -12,6 +13,8 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Camera cm;
     private Camera.Parameters p;
     private boolean avail = true;
+    private GestureDetectorCompat mDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sw = (Switch) findViewById(R.id.switch1);
         edt = (EditText) findViewById(R.id.action);
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
 //        try {
 //        cm = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
@@ -49,15 +55,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         edt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
             public void afterTextChanged(Editable editable) {
@@ -115,4 +114,33 @@ public class MainActivity extends AppCompatActivity {
             sw.setEnabled(false);
         }
     }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            if (Math.abs(velocityY) > 5000) {
+                if (velocityY < 0){
+                    setChecked(true);
+                    setLight(true);
+                    Log.w("TAG", "onFling: On" );
+                } else {
+                    setChecked(false);
+                    setLight(false);
+                    Log.w("TAG", "onFling: Off" );
+
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
 }
